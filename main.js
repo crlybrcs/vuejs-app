7704d20314b1abf69a04402c125da19d41040b1b
Tatiana <tatyana_@outlook.com>
2020-04-17 22:39:42 +0200

    update

--- main.js
+++ main.js
@@ -2,7 +2,42 @@ Vue.config.devtools = true;
 var app = new Vue({
 	el: '#app',
 	data: {
+		brand: 'Vue Mastery',
 		product: 'Socks',
-		description: 'A pair of warm, fuzzy socks'
+		selectedVariant: 0,
+		details: [ '80% cotton', '20% polyester', 'Gender-neutral' ],
+		variants: [
+			{
+				varianId: 2234,
+				variantColor: 'green',
+				variantImage: './assets/vmSocks-green-onWhite.jpg'
+			},
+			{
+				varianId: 2235,
+				variantColor: 'blue',
+				variantImage: './assets/vmSocks-blue-onWhite.jpg'
+			}
+		],
+		cart: 0
+	},
+	methods: {
+		addToCart() {
+			this.cart += 1;
+		},
+		updateProduct(index) {
+			this.selectedVariant = index;
+			console.log(index);
+		}
+	},
+	computed: {
+		title() {
+			return this.brand + ' ' + this.product;
+		},
+		image() {
+			return this.variants[this.selectedVariant].variantImage;
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity;
+		}
 	}
 });
